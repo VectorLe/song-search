bdee002e7ea74b122b2a453c34382e4db6290a1c
Vector Le <vectorle@protonmail.com>
2020-06-07 16:09:22 -0700

    Updates

---
+++ precache-manifest.34f969d2091efa366f92670d0099095e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00754a4e139c40f852b44e12d0877d0d",
+    "url": "/song-search/index.html"
+  },
+  {
+    "revision": "3c220bc7940dda243d4a",
+    "url": "/song-search/static/css/main.b24890ec.chunk.css"
+  },
+  {
+    "revision": "bcca23a83b8949ec5bda",
+    "url": "/song-search/static/js/2.9646ed9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/song-search/static/js/2.9646ed9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c220bc7940dda243d4a",
+    "url": "/song-search/static/js/main.264a931d.chunk.js"
+  },
+  {
+    "revision": "0399d169cdfa282a0d5d",
+    "url": "/song-search/static/js/runtime-main.8ebe9d0b.js"
+  },
+  {
+    "revision": "b3ff8368b319adb1f1053b4e3a3e1f33",
+    "url": "/song-search/static/media/loading.b3ff8368.gif"
+  }
+]);
